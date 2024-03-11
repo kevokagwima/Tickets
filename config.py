d2692ab85353812dfd26602520016d307e2a7b25
kevin <you@example.com>
2024-03-11 16:59:32 +0300

    Updated Files

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
-  # SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://KEVINKAGWIMA/tickets?driver=ODBC+Driver+11+for+SQL+Server"
+  # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+  SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://KEVINKAGWIMA/tickets?driver=ODBC+Driver+11+for+SQL+Server"
   SECRET_KEY = os.environ.get("SECRET_KEY")
   SQLALCHEMY_TRACK_MODIFICATIONS = False
